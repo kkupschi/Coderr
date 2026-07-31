017d016f6e0dbcd47c902c80517f57c6542f06ea
kkupschi <kkupschi@gmail.com>
2026-07-31 11:06:18 +0200

    Move secret key and settings into environment variables

--- core/settings.py
+++ core/settings.py
@@ -10,22 +10,31 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r+z3fug$!71*d^!yb0&#i0mbc*ir!bb$0jw&%xrgr%3x*=)ghv'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    host
+    for host in os.environ.get('ALLOWED_HOSTS', '').split(',')
+    if host
+]
 
 
 # Application definition
